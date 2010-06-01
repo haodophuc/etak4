4d28f617d4d3a42ae959a22a868a12b6ee544b7c
cddthd <cddthd@gmail.com>
2010-06-01 16:56:33 +0000

    Added Form_CapNhatPhong

--- QLKS_TiepNhanKhach/QLKS_TiepNhanKhach/BUS/PhongBUS.cs
+++ QLKS_TiepNhanKhach/QLKS_TiepNhanKhach/BUS/PhongBUS.cs
@@ -4,6 +4,7 @@ using System.Text;
 using QLKS_TiepNhanKhach.DAO;
 using System.Data;
 using System.Data.SqlClient;
+using QLKS_TiepNhanKhach.VO;
 
 namespace QLKS_TiepNhanKhach.BUS
 {
@@ -26,6 +27,55 @@ namespace QLKS_TiepNhanKhach.BUS
             {
                 throw e;
             }
-        }   
+        }
+
+        public int Insert(PhongVO phongVO)
+        {
+            try
+            {
+                return phongDAO.Insert(phongVO);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        
+        public int Delete(string soPhong)
+        {
+            try
+            {
+                return phongDAO.Delete(soPhong);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        
+        public int Update(PhongVO phongVO)
+        {
+            try
+            {
+                return phongDAO.Update(phongVO);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        
+        public int GetNewIndentity()
+        {
+            try
+            {
+                return phongDAO.GetLastIdentity() + 1;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
--- QLKS_TiepNhanKhach/QLKS_TiepNhanKhach/Form/Form_CapNhatKhachHang.designer.cs
+++ QLKS_TiepNhanKhach/QLKS_TiepNhanKhach/Form/Form_CapNhatKhachHang.designer.cs
@@ -34,7 +34,7 @@ namespace QLKS_TiepNhanKhach
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(742, 437);
+            this.ClientSize = new System.Drawing.Size(757, 437);
             this.Name = "Form_CapNhatKhachHang";
             this.Text = "Form_CapNhatKhachHang";
             this.Load += new System.EventHandler(this.Form_CapNhatKhachHang_Load);
